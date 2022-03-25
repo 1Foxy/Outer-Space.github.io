9291be7218eea025ba6410dd361f8edfa13fa5e6
ＳｕｎＤｏｗｎ <58747230+1Foxy@users.noreply.github.com>
2022-03-25 01:02:58 +0100

    Add files via upload

---
+++ Files/title.js
@@ -0,0 +1,40 @@
+﻿if(document.addEventListener){
+	document.addEventListener("DOMContentLoaded", function(){
+		loaded();
+	});
+} else if(document.attachEvent){
+	document.attachEvent("onreadystatechange", function(){
+		loaded();
+	});
+}
+
+function loaded(){
+	
+	setInterval(loop, 250);
+
+}
+
+var x = 0;
+
+var titleText = [ "|",
+					"A|",
+					"A |",
+					"A N|",
+					"A N |",
+					"A N T|",
+					"A N T |",
+					"A N T I|",
+					"A N T I |",
+					"A N T I G|",
+					"A N T I G |",
+					"A N T I G A|",
+					"A N T I G A |",
+					"A N T I G A N|",
+					"A N T I G A N |",
+					"A N T I G A N G|",
+					"A N T I G A N G |"];
+function loop(){
+
+	document.getElementsByTagName("title")[0].innerHTML = titleText[x++%titleText.length];
+
+}
